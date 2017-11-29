351e9087d0bf94670996682aabad45c3b1b23c2f
谢育初 <524945638@qq.com>
2017-11-29 10:58:10 +0800

    CRUD admin_jobs necessary

---
+++ app/controllers/admin/jobs_controller.rb
@@ -0,0 +1,50 @@
+class Admin::JobsController < ApplicationController
+  layout "admin"
+
+  def index
+    @jobs = current_user.jobs
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def create
+    @job = Job.new(job_params)
+    @job.user = current_user
+    if @job.save
+      redirect_to admin_jobs_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to admin_jobs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to admin_jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+end
